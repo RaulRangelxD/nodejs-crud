8723c7e6bdba44974b3691ac57906790bc59f1e9
RaulRangelxD <raulrangel1230@gmail.com>
2023-10-28 21:44:21 -0400

    Video 3 (editar)

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/css', express.static(path.join(__dirname, 'views')))
 
 app.set('view engine', 'ejs');
 
