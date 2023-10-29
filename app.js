869c0f58cb6ed63538d4642d274213b6d3856902
RaulRangelxD <raulrangel1230@gmail.com>
2023-10-28 22:58:22 -0400

    agregado iconos

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
-app.use('/css', express.static(path.join(__dirname, 'views')))
+app.use("/icons", express.static(path.join(__dirname, "node_modules/boxicons")));
 
 app.set('view engine', 'ejs');
 
